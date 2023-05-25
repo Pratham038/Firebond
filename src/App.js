39da0813996d89740297ef9dcb0d862f27bdbffc
Pratham Haram <tpass588@gmail.com>
2023-05-25 17:03:08 +0530

    FC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const MyComponent = () => {
+  const [inputs, setInputs] = useState([{ variableName: '', dropdownValue: 'false' }]);
+  const [selectedInput, setSelectedInput] = useState('');
+
+  const handleAddInput = () => {
+    setInputs([...inputs, { variableName: '', dropdownValue: 'false' }]);
+  };
+
+  const handleVariableNameChange = (e, index) => {
+    const newInputs = [...inputs];
+    newInputs[index].variableName = e.target.value;
+    setInputs(newInputs);
+  };
+
+  const handleDropdownChange = (e, index) => {
+    const newInputs = [...inputs];
+    newInputs[index].dropdownValue = e.target.value;
+    setInputs(newInputs);
+  };
+
+  const handleSelectChange = (e) => {
+    setSelectedInput(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {inputs.map((input, index) => (
+        <div key={index}>
+          <label>
+            Variable Name:
+            <input
+              type="text"
+              value={input.variableName}
+              onChange={(e) => handleVariableNameChange(e, index)}
+            />
+          </label>
+          <br />
+          <label>
+            Dropdown:
+            <select
+              value={input.dropdownValue}
+              onChange={(e) => handleDropdownChange(e, index)}
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </label>
+          <hr />
+        </div>
+      ))}
+      <select value={selectedInput} onChange={handleSelectChange}>
+        {inputs.map((input, index) => (
+          <option key={index} value={`${input.variableName}:${input.dropdownValue}`}>
+            {input.variableName} - {input.dropdownValue}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleAddInput}>Add Input</button>
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
